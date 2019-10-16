155aba54cd3caff64b6cd60b6bfba9477321f96d
Sebastian Eklund Larsen <SDU\selar16@imada-106320.adm.c.sdu.dk>
2019-10-16 05:13:06 +0200

    Uncommented

--- catalan/doIt.py
+++ catalan/doIt.py
@@ -31,7 +31,7 @@ for a in dg.products: a.print() # this can take a while in the post processing
 flow = dgFlow(dg)
 flow.objectiveFunction =  DGFlowLinExp() # important, otherwise the default function will min. #edgesUsed which may take a long time
 flow.addSource(level)
-#flow.addSink(goal)
+flow.addSink(goal)
 flow.addConstraint(inFlow(level) == 1)
 flow.calc()
 flow.solutions.print(flowPrinter)
